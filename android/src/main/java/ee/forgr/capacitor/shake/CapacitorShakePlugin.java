0738756138390a8246ec7bab2c8384b917961e8d
Vittorio Lo Preiato <vittorio.falconeta@gmail.com>
2024-02-04 16:46:56 +0100

    fix android

--- android/src/main/java/ee/forgr/capacitor/shake/CapacitorShakePlugin.java
+++ android/src/main/java/ee/forgr/capacitor/shake/CapacitorShakePlugin.java
@@ -1,5 +1,8 @@
 package ee.forgr.capacitor.shake;
 
+import android.content.Context;
+import android.hardware.SensorManager;
+
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
@@ -11,6 +14,14 @@ import com.squareup.seismic.ShakeDetector;
 public class CapacitorShakePlugin
   extends Plugin
   implements ShakeDetector.Listener {
+
+  @Override
+  public void load() {
+    var shakeDetector = new ShakeDetector(this);
+    var sensorManager = (SensorManager) this.bridge.getContext().getSystemService(Context.SENSOR_SERVICE);
+    shakeDetector.start(sensorManager, SensorManager.SENSOR_DELAY_GAME);
+    super.load();
+  }
 
   @Override
   public void hearShake() {
